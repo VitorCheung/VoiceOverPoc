d4c3df654b4e248da2955ff7f3d090650f90b528
VitorCheung <Vitorcheung@gmail.com>
2021-09-20 18:08:55 -0300

    Voice Over nos botões

--- VoiceOverPoc/ViewController.swift
+++ VoiceOverPoc/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var botaoSom: UIButton!
+    @IBOutlet weak var botaoAjuda: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = true
+        botaoSom.isAccessibilityElement = true
+        botaoSom.accessibilityLabel = "Desativar som"
+        botaoAjuda.isAccessibilityElement = true
+        botaoAjuda.accessibilityLabel = "Ajuda"
     }
 
 
